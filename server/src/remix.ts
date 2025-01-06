6d88875c5c278daa33d0886b96930412e7e0792d
matthew bouchard <bouchard.matthewj@gmail.com>
2025-01-05 22:31:16 -0500

    change workspace from remix to client

--- server/src/remix.ts
+++ server/src/remix.ts
@@ -24,9 +24,9 @@ sourceMapSupport.install({
 })
 installGlobals()
 
-const BUILD_PATH = path.resolve('../remix/build/index.cjs')
-const PUBLIC_PATH = path.resolve('../remix/public')
-const VERSION_PATH = path.resolve('../remix/build/version.txt')
+const BUILD_PATH = path.resolve('../client/build/index.cjs')
+const PUBLIC_PATH = path.resolve('../client/public')
+const VERSION_PATH = path.resolve('../client/build/version.txt')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -79,17 +79,11 @@ async function createDevRequestHandler(initialBuild) {
 
 async function getRemixHandler() {
   const remixBuild = await reimportServer()
-  const remixHandler = isDev
-    ? await createDevRequestHandler(remixBuild)
-    : createRequestHandler({ build: remixBuild })
+  const remixHandler = isDev ? await createDevRequestHandler(remixBuild) : createRequestHandler({ build: remixBuild })
 
   const handler: RequestHandler = async (req, res, next) => {
     // if the request is for the API, skip Remix and let Nest handle it
-    if (
-      req.originalUrl.startsWith('/api') ||
-      req.originalUrl.startsWith('/build') ||
-      req.originalUrl.startsWith('/assets')
-    ) {
+    if (req.originalUrl.startsWith('/api') || req.originalUrl.startsWith('/build') || req.originalUrl.startsWith('/assets')) {
       return next()
     }
 
